8a228b2e8eeef3e7c8f484d8ffc2e6e928b7ca54
yaoluxing <yaoluxing@bytedance.com>
2026-01-22 22:47:32 +0800

    feat: 增加上报代码

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,6 +65,20 @@ export default function RootLayout({
             `,
           }}
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;
+                t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "v5h7no4udq");
+            `,
+          }}
+        />
         <link rel="canonical" href="https://ptataxcalculator.org" />
         <link rel="icon" href="/favicon.ico" sizes="32x32" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
